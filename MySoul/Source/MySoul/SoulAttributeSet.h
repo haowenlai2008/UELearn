e33817c59f250546f92a449af96eea4532a97eab
haowenlai2008 <719590102@qq.com>
2024-01-06 04:08:16 +0800

    GAS，加血功能

--- MySoul/Source/MySoul/SoulAttributeSet.h
+++ MySoul/Source/MySoul/SoulAttributeSet.h
@@ -2,10 +2,17 @@
 
 #pragma once
 
+#include "AbilitySystemComponent.h"
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "SoulAttributeSet.generated.h"
-#include "AbilitySystemComponent.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 /**
  * 
  */
@@ -14,4 +21,19 @@ class MYSOUL_API USoulAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	// 等级
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Level;
+	ATTRIBUTE_ACCESSORS(USoulAttributeSet, Level)
+
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(USoulAttributeSet, MaxHealth)
+
+	// 当前生命值
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(USoulAttributeSet, Health)
+
 };
